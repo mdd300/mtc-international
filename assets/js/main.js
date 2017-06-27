ee9d755ebf42b99eed1add4c611a58e3085ee515
eduruiz <contato@eduruiz.com.br>
2017-06-27 13:22:24 -0300

    ajustar js para setas do banner funcionarem com click

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@
 
 //smooth scrooling
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
+  $('a[href*="#"]:not([href="#"])').not('.carousel-control').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
