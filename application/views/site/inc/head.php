4aa0977d13e7ebbc0eb02d114681f8d3b17a845d
Vanessa de Almeida França <vafranca@gmail.com>
2017-06-29 14:08:40 -0300

    Alterar tags SEO.

--- application/views/site/inc/head.php
+++ application/views/site/inc/head.php
@@ -2,12 +2,13 @@
     <title>MTC Log</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="LOGÍSTICA PARA E-COMMERCE  - Operações Logísticas Internas e externas.">
     <?php if (isset($description) && $description != ''): ?>
         <meta name="description" content="<?= $description ?>">
     <?php else: ?>
-        <meta name="description" content="Um grupo empresarial com 20 anos de atuação no mercado nacional. Grupo MTC uma organização solida, reconhecida pela qualidade.">
+        <meta name="description" content="MTC LOG - Logística Reversa, implementação de WMS, transporte, serviços técnicos, reengenharia de embalagens de exportação e muito mais.">
     <?php endif ?>
-    <meta name="keywords" content="">
+    <meta name="keywords" content="operações, logísticas, internas, externas, armazéns, reversa, wms, transporte, serviços, técnicos, reengenharia, embalagens, sub-montagem, consultoria, locação, e-commerce, outbound, armazenagem.">
     <base href="<?= base_url() ?>">
     <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:700,600,400,300,200">
     <link type="text/css" rel="stylesheet" href="assets/libs/font-awesome/css/font-awesome.min.css">
